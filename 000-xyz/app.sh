35f068d812dfe7b91f05a8e124749b70d67d9487
xgqfrms <xgqfrms@outlook.com>
2019-05-23 08:56:47 +0800

    cmd & bash

---
+++ 000-xyz/app.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "🎉  ^v^ app is running in production building!" && npm run build
---
+++ 000-xyz/dev.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "🎉  ^v^ app is running in development env!" && npm run start
