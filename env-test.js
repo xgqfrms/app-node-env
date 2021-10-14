905241a4ecef7e18b4e5ea22c54ed68244d97f02
xgqfrms <xgqfrms@outlook.com>
2021-10-15 01:13:06 +0800

    Update env-test.js

--- env-test.js
+++ env-test.js
@@ -1,5 +1,13 @@
 // webpack.config.js
-const PORT_ENV = require('./env.js');
+// const PORT_ENV = require('./env.js');
 // Error: Cannot find module './env.js'
 
-console.log('PORT_ENV =', PORT_ENV);
+// console.log('PORT_ENV =', PORT_ENV);
+console.log('PORT_ENV =', process.env.PORT_ENV);
+
+// 临时 current child process env
+// PORT_ENV=666 node ./env-test.js
+
+// 临时 current parent process env
+// export PORT_ENV=666
+// node ./env-test.js
