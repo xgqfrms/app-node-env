2bf0774a6b78c538449ba5a9233b3af2461006b4
xgqfrms <xgqfrms@outlook.com>
2023-03-27 03:23:12 +0800

    feat: shell scripts

--- ane-esm.mjs
+++ ane-esm.mjs
@@ -1,1 +1,91 @@
-//
+#!/usr/bin/env node
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2019.05.12
+ * @modified 2023-03-26
+ *
+ * @description APP_ENV Generator
+ * @difficulty Easy
+ * @time_complexity O(n)
+ * @space_complexity O(n)
+ * @augments
+ * @example node ./env-generator.js APP_ENV=dev
+ * @link
+ * @solutions
+ *
+ * @best_solutions
+ *
+ */
+
+// import fs from "node:fs";
+import fs from 'fs';
+import * as packageInfo from "./package.json" assert { type: "json" };
+
+const {
+  version,
+  argv,
+} = process;
+
+// v18.12.0
+console.log('\n🚀 your node.js version =', version.substring(1).split('.')[0]);
+
+const args = argv.slice(2) || [];
+
+const isPureNumber = (str = '') => {
+  const numberDict = [...''.padEnd(10, 'x')].map((_, i) => `${i}`);
+  const arr = `${str}`.split('');
+  return arr.every(item => numberDict.includes(item));
+}
+
+let key = "";
+let env = "";
+
+if (!args[0]) {
+  args[0] = "";
+  console.log(`📦 version = ${packageInfo.version}`);
+  key = 'APP_ENV';
+  env = 'dev';
+} else {
+  const tempArr = args[0].split('=');
+  key = tempArr[0] || 'APP_ENV';
+  env = isPureNumber(tempArr[1]) ? parseInt(tempArr[1]) : tempArr[1];
+}
+
+let defaultTemplate = ``;
+let envTemplate = ``;
+
+// ES6
+if (isPureNumber(env)) {
+  defaultTemplate = `// default\nconst ${key} = ${env};\nexport default ${key};\n`;
+  envTemplate = `// 🎉\nconst ${key} = ${env};\nexport default ${key};\n`;
+} else {
+  defaultTemplate = `// default\nconst ${key} = '${env}';\nexport default ${key};\n`;
+  envTemplate = `// 🎉\nconst ${key} = '${env}';\nexport default ${key};\n`;
+}
+
+console.log('\n🎮 creating...');
+
+const filename = "./env.js";
+
+fs.open(filename, "r", (err, fd) => {
+  if (err) {
+    fs.writeFile(filename, defaultTemplate, (err, fd) => {
+      if (err) {
+        console.log(`writeFile err =`, err);
+      }
+    });
+  } else {
+    fs.writeFile(filename, envTemplate, (err, fd) => {
+      if (err) {
+        console.log(`writeFile err =`, err);
+      }
+    });
+  }
+});
+
+console.log('\n🎉 finished!');
+
